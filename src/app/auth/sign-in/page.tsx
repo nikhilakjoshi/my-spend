c1c8d964f539c9dcbdad3916e381baf9102da88c
Nikhil Joshi <nikhilakjoshi@gmail.com>
2024-01-09 09:33:33 -0600

    Initial commit

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/shad/components/ui/ui/button";
+import { Card, CardContent, CardHeader } from "@/shad/components/ui/ui/card";
+import { Input } from "@/shad/components/ui/ui/input";
+import { Label } from "@/shad/components/ui/ui/label";
+
 export default async function signInPage() {
   return (
-    <main className="">
-      <h1>Sign In</h1>
+    <main className="container mx-auto max-w-[480px]">
+      <Card className="rounded-md text-gray-700">
+        <CardHeader className="text-2xl font-bold">Sign in</CardHeader>
+        <CardContent className="space-y-4">
+          <div>
+            <Label htmlFor="email">
+              Email <span className="text-amber-800">&#42;</span>
+            </Label>
+            <Input id="email" type="text" />
+          </div>
+          <div>
+            <Label htmlFor="pwd">
+              Password <span className="text-amber-800">&#42;</span>
+            </Label>
+            <Input type="password" id="pwd" />
+          </div>
+          <div className="pt-8">
+            <Button className="w-full">Login</Button>
+          </div>
+        </CardContent>
+      </Card>
     </main>
   );
 }
--- src/stories/TestStory/Button/Button.tsx
+++
@@ -1,5 +0,0 @@
-import { Button as ShadButton } from "@/shad/components/ui/ui/button";
-
-export const Button = () => {
-  return <ShadButton className="bg-red-400">Button</ShadButton>;
-};
